0e9f798511e2e30e9902a45428475b44354b248d
steve <29133953+stevefan1999-personal@users.noreply.github.com>
2024-09-17 00:33:05 +0800

    use sqlite history for repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,6 +1,7 @@
 use rustyline::{
-    config::Configurer, error::ReadlineError, validate::MatchingBracketValidator, Behavior,
-    Completer, Editor, Helper, Highlighter, Hinter, Validator,
+    config::Configurer, error::ReadlineError, sqlite_history::SQLiteHistory,
+    validate::MatchingBracketValidator, Behavior, Completer, Config, Editor, Helper, Highlighter,
+    Hinter, Validator,
 };
 use tokio::{sync::mpsc, task::yield_now};
 
@@ -15,7 +16,14 @@ pub async fn run_repl(output_sink: mpsc::UnboundedSender<String>) {
         brackets: MatchingBracketValidator::new(),
     };
     let mut interrupted = false;
-    let mut rl = Editor::new().unwrap();
+    let config = Config::default();
+    let mut rl = Editor::with_history(
+        config,
+        SQLiteHistory::open(config, "history.db")
+            .or(SQLiteHistory::with_config(config))
+            .unwrap(),
+    )
+    .unwrap();
     rl.set_behavior(Behavior::PreferTerm);
     rl.set_helper(Some(h));
 
@@ -34,7 +42,7 @@ pub async fn run_repl(output_sink: mpsc::UnboundedSender<String>) {
 
                 if !text.is_empty() {
                     let _ = output_sink.send(text.clone());
-                    let _ = rl.add_history_entry(&text);
+                    let _ = rl.add_history_entry(&text).unwrap();
                 }
 
                 yield_now().await;
