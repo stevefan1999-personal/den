cd4a75e675ac30d4368fec1ea13c8fe8f5393832
steve <29133953+stevefan1999-personal@users.noreply.github.com>
2024-09-17 01:35:56 +0800

    stdlib-core: use base64-simd instead

--- den-stdlib-core/src/lib.rs
+++ den-stdlib-core/src/lib.rs
@@ -1,18 +1,18 @@
 #[rquickjs::module(rename = "camelCase", rename_vars = "camelCase")]
 pub mod core {
-    use base64::prelude::*;
+    use base64_simd::STANDARD;
     use rquickjs::{module::Exports, Coerced, Ctx};
 
     pub use crate::cancellation::CancellationTokenWrapper;
 
     #[rquickjs::function()]
     pub fn btoa(value: Coerced<String>) -> rquickjs::Result<String> {
-        Ok(BASE64_STANDARD.encode(value.as_bytes()))
+        Ok(STANDARD.encode_to_string(value.as_bytes()))
     }
 
     #[rquickjs::function()]
     pub fn atob<'js>(ctx: Ctx<'js>, value: Coerced<String>) -> rquickjs::Result<String> {
-        match BASE64_STANDARD.decode(value.as_bytes()) {
+        match STANDARD.decode_to_vec(value.as_bytes()) {
             Ok(decoded) => Ok(String::from_utf8(decoded)?),
             Err(e) => Err(rquickjs::Exception::throw_internal(&ctx, &format!("{e}"))),
         }
